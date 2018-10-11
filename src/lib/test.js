5a2121e2f735e23f04a5d8228c233b7fd6e9aa20
kiranov <kiranov.dm@phystech.edu>
2018-10-11 21:30:36 +0300

    HW1

---
+++ src/lib/test.js
@@ -0,0 +1,8 @@
+const say = function (name) {
+  const d = document.createElement('div');
+  d.innerHTML = `Hello ${name}`;
+  document.body.appendChild(d);
+  d.setAttribute('class', 'div_one');
+  return `Hello ${name}`;
+};
+export default say;
